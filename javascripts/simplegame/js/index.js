1c4b35525b26cecfe56781c42a6b7d200ca977a9
mrdatngo <mrdatngo@gmail.com>
2020-11-30 22:27:40 +0700

    js day 8

--- javascripts/simplegame/js/index.js
+++ javascripts/simplegame/js/index.js
@@ -8,42 +8,79 @@ imgElem.src = "images/player.png";
 var screenWidth = 500;
 var screenHeigh = 500;
 
+function clearScreen() {
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(0, 0, screenWidth, screenHeigh);
+}
+
 class Player {
-    constructor(x, y) {
+    constructor(x, y, width, heigh, step) {
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.heigh = heigh;
+        this.step = step;
     }
 
     moveDown = () => {
-        ctx.fillStyle = "#FFF";
-        ctx.fillRect(0, 0, 500, 500);
-        player.y += 50;
-        ctx.drawImage(imgElem, player.x, player.y, 50, 50);
+        this.y += this.step;
     };
 
     moveRight = () => {
-        ctx.fillStyle = "#FFF";
-        ctx.fillRect(0, 0, 500, 500);
-        player.x += 50;
-        ctx.drawImage(imgElem, player.x, player.y, 50, 50);
+        this.x += this.step;
+    };
+
+    moveUp = () => {
+        if (this.y >= this.step) {
+            this.y -= this.step;
+        }
+    };
+
+    moveLeft = () => {
+        if (this.x >= this.step) {
+            this.x -= this.step;
+        }
+    };
+
+    draw = (ctx) => {
+        ctx.drawImage(imgElem, this.x, this.y, this.width, this.heigh);
     };
 }
 
-var player = new Player(0, 0);
+var player = new Player(0, 0, 50, 50, 2);
+// var player2 = new Player(250, 250);
 
 window.onload = () => {
     ctx.drawImage(imgElem, player.x, player.y, 50, 50);
 };
 
 document.addEventListener("keydown", (event) => {
-    console.log("event: ", event, event.code);
+    // console.log("event: ", event, event.code);
     switch (event.code) {
+        // case "KeyS":
+        //     player2.moveDown();
+        //     break;
+        // case "KeyD":
+        //     player2.moveRight();
+        //     break;
         case "ArrowDown":
-            // player_moving_down();
             player.moveDown();
             break;
         case "ArrowRight":
             player.moveRight();
             break;
+        case "ArrowLeft":
+            player.moveLeft();
+            break;
+        case "ArrowUp":
+            player.moveUp();
+            break;
     }
 });
+
+// fps = frame per second
+setInterval(() => {
+    clearScreen();
+    player.draw(ctx);
+    // player2.draw(ctx);
+}, 1);
