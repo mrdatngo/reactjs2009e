2086b425e26c746a9ee66e3b76d59fc853461ddc
mrdatngo <mrdatngo@gmail.com>
2020-12-02 20:29:57 +0700

    route to treasure

--- javascripts/simplegame/js/index.js
+++ javascripts/simplegame/js/index.js
@@ -9,45 +9,59 @@ var screenWidth = 500;
 var screenHeigh = 500;
 
 function clearScreen() {
-    ctx.fillStyle = "#FFF";
+    ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, screenWidth, screenHeigh);
 }
 
 class Player {
-    constructor(x, y, width, heigh, step) {
+    constructor(x, y, width, heigh, step, image) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.heigh = heigh;
         this.step = step;
+        this.image = image;
+        this.gold = 0;
     }
 
     moveDown = () => {
-        this.y += this.step;
+        if (this.y + this.heigh + this.step <= screenHeigh) {
+            this.y += this.step;
+        }
+        checkWin();
     };
 
     moveRight = () => {
-        this.x += this.step;
+        if (this.x + this.width + this.step <= screenHeigh) {
+            this.x += this.step;
+        }
+        checkWin();
     };
 
     moveUp = () => {
         if (this.y >= this.step) {
             this.y -= this.step;
         }
+        checkWin();
     };
 
     moveLeft = () => {
         if (this.x >= this.step) {
             this.x -= this.step;
         }
+        checkWin();
+    };
+
+    goldCollected = (number) => {
+        this.gold += number;
     };
 
     draw = (ctx) => {
-        ctx.drawImage(imgElem, this.x, this.y, this.width, this.heigh);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.heigh);
     };
 }
 
-var player = new Player(0, 0, 50, 50, 2);
+var player = new Player(0, 0, 50, 50, 50, imgElem);
 // var player2 = new Player(250, 250);
 
 window.onload = () => {
@@ -78,9 +92,75 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
+// create map
+var trapImg = document.createElement("img");
+trapImg.src = "images/trap.png";
+var goldImg = document.createElement("img");
+goldImg.src = "images/treasure.png";
+
+var map = [
+    [0, 0, 0, -1, 0, 1, 0, 1, 0, 0],
+    [0, 0, -1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, -1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, -1, 0, 1, 0, 1, 1, 0],
+    [0, -1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, -1, 1, 1, 0],
+    [0, 0, 0, -1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, -1, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, -1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, -1, 0, 1, 0, 1, 0, 0],
+];
+
+var storeMap = map.map((cells) => cells.slice());
+console.log("storeMap: ", storeMap);
+// rule: -1: trap, 0: nothing, 1: gold
 // fps = frame per second
+
+var numberOfGold = 0;
+map.forEach((cells) => {
+    cells.forEach((cell) => {
+        if (cell === 1) {
+            numberOfGold++;
+        }
+    });
+});
+
+function checkWin() {
+    // check player collect gold
+    const playerYIndex = player.x / 50;
+    const playerXIndex = player.y / 50;
+    if (map[playerXIndex][playerYIndex] === 1) {
+        player.goldCollected(1);
+        map[playerXIndex][playerYIndex] = 0;
+    }
+
+    if (map[playerXIndex][playerYIndex] === -1) {
+        alert("You lose!");
+        // reset game
+        map = storeMap;
+        player.x = 0;
+        player.y = 0;
+    }
+
+    if (player.gold === numberOfGold) {
+        alert("You win!");
+    }
+}
+
 setInterval(() => {
     clearScreen();
     player.draw(ctx);
     // player2.draw(ctx);
-}, 1);
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            if (map[i][j] === -1) {
+                // draw a trap
+                // console.log("i, j", i, j);
+                ctx.drawImage(trapImg, j * 50, i * 50, 50, 50);
+            }
+            if (map[i][j] === 1) {
+                ctx.drawImage(goldImg, j * 50, i * 50, 50, 50);
+            }
+        }
+    }
+}, 50);
